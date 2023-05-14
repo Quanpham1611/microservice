a344bcc3e76a0d08becf498fde00513aff1368a6
quan <qpham16112003@gmail.com>
2023-05-14 08:30:36 +0700

    1st init

---
+++ ecommerce-microservice/api-gateway/src/test/java/quan/ecoomerce/apigateway/ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package quan.ecoomerce.apigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
